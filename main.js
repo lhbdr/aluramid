c1dbcd6e009a145634ce2e9c4e3605a8bd49e5db
lhbdr <106272647+lhbdr@users.noreply.github.com>
2023-10-05 09:08:14 -0300

    05/10

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 alert('Seja bem-vindo')
+
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+function tocaSomPom(){Document.querySelector('som_tecla_pom').play();
+}
+document.querySelector('.tecla_tim').onclick = tocaSomtim;
+function tocaSomTim(){Document.querySelector('som_tecla_tim').play();
+}
+document.querySelector('tecla_puff').onclick = tocaSompuff;
+function tocaSompuff(){Document.querySelector('som_tecla_puff').play();
+}
+document.querySelector('tecla_splash').onclick = tocaSomsplash;
+function tocaSomsplash(){Document.querySelector('som_tecla_splash').play();
+}
+document.querySelector('tecla_toim').onclick = tocaSomtoim;
+function tocaSomtoim(){Document.querySelector('som_tecla_toim').play();
+}
+document.querySelector('tecla_psh').onclick = tocaSompsh;
+function tocaSompsh(){Document.querySelector('som_tecla_psh').play();
+}
+document.querySelector('tecla_tic').onclick = tocaSomtic;
+function tocaSomtic(){Document.querySelector('som_tecla_tic').play();
+}
+document.querySelector('tecla_tom').onclick = tocaSomtom;
+function tocaSomtom(){Document.querySelector('som_tecla_tom').play();
+}
+document.querySelector('tecla_clap').onclick = tocaSomclap;
+function tocaSomclap(){Document.querySelector('som_tecla_clap').play();
+}
